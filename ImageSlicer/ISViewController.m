f9d9cc8951464e30a9b20ad9c5fd48957f4fa1eb
Markus Emrich <Markus.Emrich@gmail.com>
2013-10-26 13:37:48 +0100

    added basic drawing logic

--- ImageSlicer/ISViewController.m
+++ ImageSlicer/ISViewController.m
@@ -19,18 +19,63 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
-    NSArray *images = @[[UIImage imageNamed:@"01"],
-                        [UIImage imageNamed:@"02"],
-                        [UIImage imageNamed:@"03"],
-                        [UIImage imageNamed:@"04"]];
-    UIImage *image = [self imageFromSourceImages:images];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // get source images
+    NSArray *images = @[[UIImage imageNamed:@"01.jpg"],
+                        [UIImage imageNamed:@"02.jpg"],
+                        [UIImage imageNamed:@"03.jpg"],
+                        [UIImage imageNamed:@"04.jpg"]];
+    
+    // create new image
+    UIImage *image = [self imageFromSourceImages:images
+                                     stripeWidth:3
+                                         pattern:@[@(0),@(1),@(2),@(1),@(0),@(3)]];
+    
+    // save image
+    if(image) {
+        NSString *path = [[NSFileManager defaultManager] desktopPathForDirectory:@"ImageSlicer-Results"];
+        path = [path stringByAppendingPathComponent:@"SlicedImage.jpg"];
+        NSData *imageData = UIImageJPEGRepresentation(image, 0.8);
+        [imageData writeToFile:path atomically:YES];
+    }
 }
 
-- (UIImage*)imageFromSourceImages:(NSArray*)images;
+- (UIImage*)imageFromSourceImages:(NSArray*)images
+                      stripeWidth:(NSInteger)stripeWidth
+                          pattern:(NSArray*)pattern;
 {
+    if (images.count == 0 || stripeWidth <= 0 || pattern.count == 0) return nil;
     
+    // get size of big image
+    UIImage *firstImage = images[0];
+    CGSize size = firstImage.size;
+    size.width *= images.count;
+    
+    // draw image stripes
+    UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+    
+    NSInteger xpos = 0;
+    NSInteger patternIndex = 0;
+    while (xpos < size.width) {
+        
+        // find correct image
+        NSInteger imageIndex = [pattern[patternIndex] intValue];
+        UIImage *image = images[imageIndex];
+        
+        // draw image
+        [image drawAtPoint:CGPointMake(xpos,0)];
+        
+        // increment
+        patternIndex = (patternIndex+1) % pattern.count;
+        xpos += stripeWidth;
+    }
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
 }
 
 @end
