60a16073cd52c3b817bd925f84b0ffe1eb2d5409
Markus Emrich <Markus.Emrich@gmail.com>
2013-10-26 13:14:45 +0100

    project setup

--- ImageSlicer/ISAppDelegate.m
+++ ImageSlicer/ISAppDelegate.m
@@ -6,10 +6,19 @@
 //  Copyright (c) 2013 nxtbgthng. All rights reserved.
 //
 
+#import "ISViewController.h"
+
 #import "ISAppDelegate.h"
 
 @implementation ISAppDelegate
 
-
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
+{
+    self.window = [[UIWindow alloc] init];
+    self.window.rootViewController = [[ISViewController alloc] init];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
 
 @end
